71c840fa5b7720ed7a8a11b4f537f85d1e77ae92
eduobs <edu_ob1@hotmail.com>
2015-05-28 15:08:46 -0300

    Definição da modal e formulário

--- ValidaCartoes/ValidaCartoes/Controllers/HomeController.cs
+++ ValidaCartoes/ValidaCartoes/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ValidaCartoes.Models;
 
 namespace ValidaCartoes.Controllers
 {
@@ -13,18 +14,13 @@ namespace ValidaCartoes.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(CartaoCredito model)
         {
-            ViewBag.Message = "Your application description page.";
+            model.bandeiraCartao = bandeira.VISA;
+            model.valido = true;
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(model);
         }
     }
 }
---
+++ ValidaCartoes/ValidaCartoes/Models/CartaoCredito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ValidaCartoes.Models
+{
+    public class CartaoCredito
+    {
+        public string numeroCartao { get; set; }
+        public bandeira bandeiraCartao { get; set; }
+        public bool valido { get; set; }
+    }
+
+    public enum bandeira {
+        [Description("Desconhecido")]
+        DESCONHECIDO = 0,
+        [Description("VISA")]
+        VISA = 1,
+        [Description("MasterCard")]
+        MASTERCARD = 2,
+        [Description("Discover")]
+        DISCOVER = 3,
+        [Description("AMEX")]
+        AMEX = 4
+    }
+}
